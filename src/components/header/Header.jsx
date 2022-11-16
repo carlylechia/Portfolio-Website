fa20adac442af6b7a714f187cc20e445488e452f
Chia Carlyle <chiacarlyle@gmail.com>
2022-11-17 00:08:21 +0100

    Add general app styling and header content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+    <div className="container header__container">
+      <h5>Hello, I am</h5>
+      <h1>Chia Carlyle</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+    </div>
+    <CTA />
+   </header>
   )
 }
 
