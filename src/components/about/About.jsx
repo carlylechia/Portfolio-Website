172e066d3a2af4bb86d1ca7dbc0e519b5b33ad8e
Chia Carlyle <chiacarlyle@gmail.com>
2022-11-21 16:46:36 +0100

    Finish the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import me from '../../assets/ChiaCarlyle.jpg'
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc'
+import './about.css';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me} alt="my pic" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Year Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>30+ Clients</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>40+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla illo voluptatibus voluptatem harum quos, voluptates maiores ipsam praesentium aspernatur dolorum, quia beatae quaerat a accusamus omnis quod dolorem unde magnam.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
