0692bf4fb5a442676d8f4ef6c7d6d1e4a8a33bc9
Chia Carlyle <chiacarlyle@gmail.com>
2022-12-09 13:15:15 +0100

    Finish adding content to services and fix styling

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import './services.css';
 import {BiCheck} from 'react-icons/bi';
-import {AiFillCloseCircle} from 'react-icons/ai';
+import {BsChevronUp} from 'react-icons/bs';
+import {BsChevronDown} from 'react-icons/bs';
 
 const Services = () => {
   const [details, setDetails] = useState('');
@@ -18,23 +19,23 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Gather and evaluate user requirements in collaboration with product managers and engineers.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Illustrate design ideas using storyboards, process flows and sitemaps.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Design graphic user interface elements, like menus, tabs and widgets.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Identify and troubleshoot UX problems (e.g. responsiveness).</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Adhere to style standards on fonts, colors and images.</p>
             </li>
           </ul>
         </article>
@@ -47,36 +48,40 @@ const Services = () => {
           </div>
           <ul className="service__list">
             <li>
-              <BiCheck className='service__list-icon' onClick={() => {setDetails('first');}}/>
+              <BiCheck className='service__list-icon' />
               <span>
-                <p>Custom Website Development</p>
-                <small className={details === 'first' ? 'show' : 'no-show'}>I can build your website from scratch. All you need to do is provide me all the data I need, and consider it done.
-                <AiFillCloseCircle onClick={() => {setDetails('');}}/></small>
+                <p className={details === 'first' ? 'title' : ''}>Custom Website Development</p>
+                <small className={details === 'first' ? 'show' : 'no-show'}>I can build your website from scratch. All you need to do is provide me all the data I need, and consider it done. To start from scratch, you will need a complete and comprehensive SWOT analysis of your business and your competitors. Find out what makes your rival website stand out and compare this to your strengths and weaknesses. On top of that, you will also need the logo, design, color branding, and images.
+                <BsChevronUp className='service__arrows' title='show less' onClick={() => {setDetails('');}}/></small>
               </span>
+              <BsChevronDown className={details === 'first' ? 'no-show' : 'show service__arrows'} title='show more' onClick={() => {setDetails('first');}}/>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <span>
+                <p className={details === 'second' ? 'title' : ''}>Thorough Website Maintenance</p>
+                <small className={details === 'second' ? 'show' : 'no-show'}>Already have a website? Great! Your customers would not want to stay on a stale homepage. So, you have to keep your website fresh, trendy, and updated; and I can help you do that. Thorough website maintenance can potentially increase traffic, generate more leads, rank your website better, and build a more reliable brand.
+                <BsChevronUp className='service__arrows' title='show less' onClick={() => {setDetails('');}}/></small>
+              </span>
+              <BsChevronDown className={details === 'second' ? 'no-show' : 'show service__arrows'} title='show more' onClick={() => {setDetails('second');}}/>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <span>
+                <p className={details === 'third' ? 'title' : ''}>E-commerce Website Development</p>
+                <small className={details === 'third' ? 'show' : 'no-show'}> Do you have a local business and are seeking to go digital? I am available to build your eCommerce site, which will be your online shop, and the online home of your business.
+                <BsChevronUp className='service__arrows' title='show less' onClick={() => {setDetails('');}}/></small>
+              </span>
+              <BsChevronDown className={details === 'third' ? 'no-show' : 'show service__arrows'} title='show more' onClick={() => {setDetails('third');}}/>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <span>
+                <p className={details === 'fourth' ? 'title' : ''}>Systems and Database Management</p>
+                <small className={details === 'fourth' ? 'show' : 'no-show'}> Does your company deal in heavy data which constantly has to be managed, updated, and evaluated? Do you want to constantly improve your website's queries so as to make your site faster, and improve general user experience? I am your guy.
+                <BsChevronUp className='service__arrows' title='show less' onClick={() => {setDetails('');}}/></small>
+              </span>
+              <BsChevronDown className={details === 'fourth' ? 'no-show' : 'show service__arrows'} title='show more' onClick={() => {setDetails('fourth');}}/>
             </li>
           </ul>
         </article>
@@ -90,23 +95,23 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Research industry-related topics.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Prepare well-structured drafts using digital publishing platforms.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Create and distribute marketing copy to advertise our company and products.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Edit and proofread written pieces before publication.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+              <p>Conduct keyword research and use SEO guidelines to optimize content.</p>
             </li>
           </ul>
         </article>
