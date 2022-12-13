3c433948fe9dae663c1fd2f5e5d6e12673f2b4ce
Chia Carlyle <chiacarlyle@gmail.com>
2022-12-13 19:46:46 +0100

    Make the navbar change dynamically on scroll

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,16 +5,20 @@ import {RiServiceLine} from 'react-icons/ri';
 import {BiMessageSquareDetail} from 'react-icons/bi';
 import './nav.css';
 import { useState } from 'react';
+import { useScrollPosition } from '../../hooks/useScrollPosition';
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#home');
+  const scrollPosition = useScrollPosition();
+
+  console.log(scrollPosition);
   return (
     <nav>
-      <a href="#home" className={activeNav === '#home' ? 'active' : ''} onClick={() => setActiveNav('#home')}><AiOutlineHome /></a>
-      <a href="#about" className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><BiUser/></a>
-      <a href="#experience" className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBook/></a>
-      <a href="#services" className={activeNav === '#services' ? 'active' : ''} onClick={() => setActiveNav('#services')}><RiServiceLine/></a>
-      <a href="#contact" className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail/></a>
+      <a href="#home" className={scrollPosition <= 750 ? 'active' : ''} onClick={() => setActiveNav('#home')}><AiOutlineHome /></a>
+      <a href="#about" className={scrollPosition <= 1444 && scrollPosition > 750 ? 'active' : ''} onClick={() => setActiveNav('#about')}><BiUser/></a>
+      <a href="#experience" className={scrollPosition <= 2034 && scrollPosition > 1444 ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBook/></a>
+      <a href="#services" className={scrollPosition <= 4446 && scrollPosition >= 2034 ? 'active' : ''} onClick={() => setActiveNav('#services')}><RiServiceLine/></a>
+      <a href="#contact" className={scrollPosition > 4446 ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail/></a>
     </nav>
   )
 }
